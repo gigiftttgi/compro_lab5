e992a12565b0951fa537eea3559e283f69514de1
lab314-13@labeng.local <lab314-13@labeng.local>
2020-12-15 08:44:05 +0000

    fill out lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int even,odd,i=1;
+    while(i!=0)
+    {
+        cout << "Enter an integer: ";
+        cin >> i;
+        if (i%2==0&&i!=0)
+            even++;
+        else
+            odd++;
+    }
+    odd=odd-2;
+    cout << "#Even numbers = "<<even;
+    cout << "\n#Odd numbers = "<<odd;
     return 0;
 }
